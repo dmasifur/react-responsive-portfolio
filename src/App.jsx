372b84090fa1acdea76f99b2d1d98200551d8084
asifurrahman <dmasifur@gmail.com>
2024-08-25 02:28:43 +0600

    Starting point of the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Hero from "./sections/Hero/Hero";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
+
+export default App;
