1167cb54b5f8d95dd955a48137af6dad9a73e082
asifurrahman <dmasifur@gmail.com>
2024-08-25 18:28:46 +0600

    contact form ready need to added the action link for the functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./sections/Hero/Hero";
 import "./App.css";
 import { Projects } from "./projects/Projects";
 import Skills from "./sections/Skills/Skills";
+import Contact from "./sections/Contact/Contact";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Projects />
       <Skills />
+      <Contact />
     </>
   );
 }
--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -1,5 +1,5 @@
 import styles from "./SkillsStyles.module.css";
-import checkMarkIcon from "../../assets/checkmark-light.svg";
+import checkMarkIcon from "../../assets/checkmark-dark.svg";
 import SkillList from "../../common/SkillList";
 
 const Skills = () => {
